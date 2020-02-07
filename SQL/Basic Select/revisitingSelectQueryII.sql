11f8412b02d7fbf3a00846148a9a16c25998fd2b
DJ Lott <30908054+Novajed@users.noreply.github.com>
2020-02-06 22:02:42 -0500

    Update revisitingSelectQueryII.sql

--- SQL/Basic Select/revisitingSelectQueryII.sql
+++ SQL/Basic Select/revisitingSelectQueryII.sql
@@ -1,3 +1,9 @@
+/*
+github.com/Novajed
+
+https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+*/
+
 SELECT NAME 
 FROM CITY 
 WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
